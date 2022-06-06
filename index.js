c91dfad28a74e5fd45678fbcbc8af181896b3f9b
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-06-06 17:41:10 +0300

    commit tree 1e999462f6eec15794effbee10faba404538718b

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Kihahu Mutaga';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const charact = currentUser.charAt(0);
+const shortGreeting = `Welcome, ${charact}!`;
